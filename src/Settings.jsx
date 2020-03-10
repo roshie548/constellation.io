7b95e7a40a2d088fab42d357167da122037a8687
Ada Hu <adahu@berkeley.edu>
2020-03-10 16:17:25 -0700

    update settings

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -1,27 +1,49 @@
 import React from 'react';
-
+import "./Timer.css"
 class Settings extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.displaySettings.bind(this);
     this.state = {
       display: false,
-      websites: []
+      websites: ["facebook.com", "instagram.com"]
     }
-    this.displaySettings.bind(this);
   }
 
-  displaySettings() {
+  //should add functions to add/delete websites
+
+  displaySettings = () => { //if they click on the "SETTINGS"  button
     this.setState({display: true});
     return;
   }
 
+  closeSettings = () => {
+    this.setState({display: false});
+    return;
+  }
+
   render() {
-    return (
-      <div>
-          <button onClick={this.displaySettings}>SETTINGS</button>
-      </div>
-    );
+    if (!this.state.display) { //display the settings button only
+      return (
+        <div>
+            <button onClick={this.displaySettings}>SETTINGS</button>
+        </div>
+      );
+    }
+
+    else {
+      return ( //open the display settings
+        <div className="Settings">
+            <button onClick={this.closeSettings}>close</button>
+            <h3> Blocked Websites </h3>
+            {this.state.websites} // need to figure out how to list them?
+                                  // also how to add/delete websites
+        </div>
+      );
+    }
+
   }
 }
 
 export default Settings;
+
