2e1e3b1a4fea7d4116c2fb150caf974e56a696d4
Michelle Lin <michellealin@berkeley.edu>
2020-03-16 18:15:36 -0700

    timer works?

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -5,22 +5,53 @@ class Timer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      start_time: 0,
-      elapsed_time: 0
+      start_time: 10,
+      elapsed_time: 0,
+      active: false
     }
-    this.startTimer.bind(this);
+    //this.startTimer.bind(this);
   }
 
-  startTimer() {
-    return;
+  // startTimer() {
+  //   this.active = true
+  //   return;
+  // }
+
+  // resetTimer() {
+  //   this.active = false
+  //   return;
+  // }
+
+  componentDidMount() {
+    this.myInterval = setInterval(() => {
+      const { start_time, elapsed_time, active } = this.state
+
+      if (elapsed_time < start_time) {
+        this.setState(({ elapsed_time }) => ({
+          elapsed_time: elapsed_time + 1
+        }))
+      }
+      
+
+      else {
+        clearInterval(this.myInterval)
+      }
+      
+    }
+    , 1000)
   }
-  resetTimer() {
-    return;
+
+  componentWillUnmount() {
+    clearInterval(this.myInterval)
   }
+
+  
 
   render() {
     return (
       <div>
+
+          {this.state.elapsed_time}
           {this.state.start_time}
           <button onClick={this.startTimer} > START </button>
           <button onClick={this.resetTimer} > RESET </button>
